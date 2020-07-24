b318f8eb25a14ca99c2a93e95e4d88b693f2fadd
unknown <binibakshi@gmail.com>
2020-07-24 14:17:37 +0300

    add erver.js file

---
+++ server.js
@@ -0,0 +1,10 @@
+// server.js
+var express = require('express');
+// eslint-disable-next-line no-unused-vars
+var path = require('path');
+var serveStatic = require('serve-static');
+var app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
